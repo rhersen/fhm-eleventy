d31bd3d433d441deaafca84916279b2e7bdd8a69
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-03-10 08:13:14 +0100

    implement the full color scheme

--- src/addColor.js
+++ src/addColor.js
@@ -4,6 +4,17 @@ module.exports = (arrays) =>
   _.map(arrays, (array) =>
     _.map(array, (value) => ({
       value,
-      bgcolor: value < 60 ? "#f1b73c" : "#e79402",
+      bgcolor: bgcolor(value),
     }))
   );
+
+function bgcolor(value) {
+  if (value < 0.1) return "#66bfc6";
+  if (value < 20) return "#f5d664";
+  if (value < 60) return "#f1b73c";
+  if (value < 120) return "#e79402";
+  if (value < 240) return "#da6500";
+  if (value < 480) return "#d23f00";
+  if (value < 960) return "#b61c00";
+  else return "#870202";
+}
--- test/addColor.test.js
+++ test/addColor.test.js
@@ -5,40 +5,21 @@ describe("addColor", function () {
   it("adds color", () =>
     expect(
       subject([
-        [90, 10],
-        [60, 360],
-        [720, 180],
+        [90, 10, 0, 360],
+        [720, 180, 40, 999],
       ])
     ).to.deep.equal([
       [
-        {
-          bgcolor: "#e79402",
-          value: 90,
-        },
-        {
-          bgcolor: "#f1b73c",
-          value: 10,
-        },
+        { bgcolor: "#e79402", value: 90 },
+        { bgcolor: "#f5d664", value: 10 },
+        { bgcolor: "#66bfc6", value: 0 },
+        { bgcolor: "#d23f00", value: 360 },
       ],
       [
-        {
-          bgcolor: "#e79402",
-          value: 60,
-        },
-        {
-          bgcolor: "#e79402",
-          value: 360,
-        },
-      ],
-      [
-        {
-          bgcolor: "#e79402",
-          value: 720,
-        },
-        {
-          bgcolor: "#e79402",
-          value: 180,
-        },
+        { bgcolor: "#b61c00", value: 720 },
+        { bgcolor: "#da6500", value: 180 },
+        { bgcolor: "#f1b73c", value: 40 },
+        { bgcolor: "#870202", value: 999 },
       ],
     ]));
 });
